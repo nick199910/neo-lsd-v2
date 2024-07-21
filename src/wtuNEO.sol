e7b3883d8b7c64e9bcaba62cc60bcde9cdc82e92
nick199910 <2413810931@qq.com>
2024-07-21 14:59:24 +0800

    dele unuse contract

--- src/wtuNEO.sol
+++ src/wtuNEO.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity >=0.8.0;
-import {Test, console} from "forge-std/Test.sol";
 // Forked from Frax Finance: https://github.com/FraxFinance
 
 /// @notice Modern and gas efficient ERC20 + EIP-2612 implementation.
